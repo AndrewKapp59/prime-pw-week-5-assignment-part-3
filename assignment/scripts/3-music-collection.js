a91d44584f7509c8cc412c9273d5e7d1844a6559
Andrew Kapp <dkapp59@gmail.com>
2022-01-14 18:39:38 -0600

    Added details for 6 records

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,52 +4,56 @@ let collection = [];
 
 console.log(collection, 'I have', collection.length, 'records in my collection');
 
+function  addToCollection (record) {
+  collection.push(record);
+}
+
+
+
+addToCollection (record0);
+
+console.log(collection, 'I have', collection.length, 'record in my collection');
+
+//Record List
+
 let record0 = {
   title: 'Costello Music',
   artist: 'The Fratellis',
   yearPublished: 2007,
 }
 
-function  addToCollection (record) {
-  collection.push(record);
-}
-
-addToCollection (record0);
-
-console.log(collection, 'I have', collection.length, 'record in my collection');
-
 let record1 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'El Camino',
+  artist: 'The Black Keys',
+  yearPublished: 2011,
 }
 
 let record2 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'Brothers',
+  artist: 'The Black Keys',
+  yearPublished: 2010,
 }
 
 let record3 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'Heart It Races',
+  artist: 'Dr Dog',
+  yearPublished: 2007,
 }
 
 let record4 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'Classics',
+  artist: 'Ratatat',
+  yearPublished: 2006,
 }
 
 let record5 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'My First Car',
+  artist: 'Vulfpeck',
+  yearPublished: 2013,
 }
 
 let record6 = {
-  title: '',
-  artist: '',
-  yearPublished: '',
+  title: 'Black Sands',
+  artist: 'Bonobo',
+  yearPublished: 2010,
 }
