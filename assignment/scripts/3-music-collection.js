6c26a69199daceb07c066f72f4a6348c1b142103
Andrew Kapp <dkapp59@gmail.com>
2022-01-14 18:03:56 -0600

    working on and testing addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,55 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+console.log(collection);
+
+let record0 = {
+  title: 'Costello Music',
+  artist: 'The Fratellis',
+  yearPublished: 2007,
+}
+
+function  addToCollection (record) {
+  collection.push(record);
+}
+
+addToCollection (record0);
+
+console.log(collection);
+
+let record1 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
+
+let record2 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
+
+let record3 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
+
+let record4 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
+
+let record5 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
+
+let record6 = {
+  title: '',
+  artist: '',
+  yearPublished: '',
+}
